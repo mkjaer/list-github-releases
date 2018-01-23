b1790678213f51cc423c577a239a657b79f589b4
Mikael Kjaer <mikaelk84@gmail.com>
2018-01-23 08:12:16 +0400

    Added alertmanager, node exporter and unifi controller

--- check_github_releases.sh
+++ check_github_releases.sh
@@ -8,7 +8,7 @@ cd $(dirname $0)
 
 for REPO in $(cat repos.txt); do 
  echo -n "Latest "${REPO}" version is: " >> output.html 
- curl "${GITHUB_TOKEN}" --silent https://api.github.com/repos/"${REPO}"/releases | jq '.[] .tag_name' | grep -viE "rc|beta|alpha|zstd" | sort -rV | head -n1 >> output.html
+ curl "${GITHUB_TOKEN}" --silent https://api.github.com/repos/"${REPO}"/releases | jq -r '.[] | .tag_name + " (" + .published_at + ")"' | grep -viE "rc|beta|alpha|zstd" | sort -rV | head -n1 >> output.html
 done
 
 echo ""
